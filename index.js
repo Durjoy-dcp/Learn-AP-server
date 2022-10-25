2dcebacdd3859649427d3cd4d4e99c6e2a9057b4
Durjoy-dcp <unknownccxv@gmail.com>
2022-10-26 01:26:07 +0600

    json updated

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const app = express()
 const port = 5000
 const courses = require('./data/courses.json')
 const catagories = require('./data/catagory.json');
+const cors = require('cors')
+
+app.use(cors())
 app.get('/courses/:id', (req, res) => {
     const CatagoryId = req.params.id;
     const catagoryCourses = courses.filter(c => c.catagory === req.params.id);
